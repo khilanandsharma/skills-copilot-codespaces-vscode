a772e31c3ae604063e668b4858f7786180504bbe
khilanandsharma <khilanand.sharma@gmail.com>
2025-02-25 11:33:18 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file.
+// The comments.html file should be in the same directory as comments.js.
+// The comments.html file should contain the following HTML:
+// <!DOCTYPE html> 
+// <html>
+// <head>   
+//   <title>Comments</title>
+// </head>
+// <body>
+
+//   <h1>Comments</h1>
+//   <ul>
+//     <li>Comment 1</li>
+//     <li>Comment 2</li>
+//     <li>Comment 3</li>
+//   </ul>
+
+// </body>
+// </html>
+// The comments.js file should contain the following code:
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
